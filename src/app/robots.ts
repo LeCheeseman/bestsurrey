10724d4e093c832aad449eae72bf34e3c8fd48e6
LeCheeseman <182329632+LeCheeseman@users.noreply.github.com>
2026-07-17 11:29:24 +0100

    Explicitly allow search crawlers in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,13 +6,29 @@
 import type { MetadataRoute } from 'next'
 import { SITE_URL } from '@/lib/site'
 
+const publicCrawlerUserAgents = [
+  'Googlebot',
+  'OAI-SearchBot',
+  'PerplexityBot',
+  'ClaudeBot',
+]
+
+const disallowedPaths = ['/search', '/admin', '/api/', '/design-lab']
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow:    '/',
-      disallow: ['/search', '/admin', '/api/', '/design-lab'],
-    },
+    rules: [
+      ...publicCrawlerUserAgents.map((userAgent) => ({
+        userAgent,
+        allow:    '/',
+        disallow: disallowedPaths,
+      })),
+      {
+        userAgent: '*',
+        allow:    '/',
+        disallow: disallowedPaths,
+      },
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
   }
 }
